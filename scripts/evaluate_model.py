1592aba025aa46b70c357ecacad3ec9d12f47446
ShashiQubit <87139518+ShashiQubit@users.noreply.github.com>
2022-10-28 10:17:30 +0530

    Update evaluate_model.py

--- scripts/evaluate_model.py
+++ scripts/evaluate_model.py
@@ -70,6 +70,7 @@ if __name__ == "__main__":
     model.embedding_manager.load(opt.embedding_path)
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    model= torch.nn.DataParallel(model)
     model = model.to(device)
 
     evaluator = LDMCLIPEvaluator(device)
